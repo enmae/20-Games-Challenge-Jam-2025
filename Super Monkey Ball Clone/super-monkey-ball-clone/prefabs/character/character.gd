b361c9e278f2126636227c9466eafdf49d095cc3
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2025-08-14 17:28:17 -0400

    Update character velocity

--- Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/character/character.gd
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/character/character.gd
@@ -1,10 +1,6 @@
 extends RigidBody3D
 
-var previous_position
-
 func _process(delta: float) -> void:
-	previous_position = position
-	
 	if Input.is_action_pressed("left"):
 		PhysicsServer3D.area_set_param(get_viewport().find_world_3d().space,
 			PhysicsServer3D.AREA_PARAM_GRAVITY_VECTOR, Vector3.RIGHT)
--- Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/ui/speed_ui/speed_ui.gd
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/ui/speed_ui/speed_ui.gd
@@ -3,9 +3,5 @@ extends CanvasLayer
 @onready var speed_value: Label = $"%Speed Value"
 
 func _process(delta: float) -> void:
-	var player = get_tree().get_first_node_in_group("player")
-
-	# var current_velocity = (player.position - player.previous_position) / delta
-	## TODO: Get proper previous position to get speed to send to ui
-	var current_speed = player.previous_position.distance_to(player.position) / delta
-	speed_value.text = str(current_speed) + " MPH"
+	var player: RigidBody3D = get_tree().get_first_node_in_group("player")
+	speed_value.text = "%d m/s" % [player.linear_velocity.length()]
