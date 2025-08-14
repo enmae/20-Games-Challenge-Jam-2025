7e2a0e84afbc376be77b863812a9a940e503d899
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2025-08-14 10:50:17 -0400

    Basic structure of game
    -Set up title screen and "greybox" level
    -Also figured out controlling gravity

---
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/character/character.gd
@@ -0,0 +1,10 @@
+extends RigidBody3D
+
+func _process(delta: float) -> void:
+	
+	if Input.is_action_pressed("left"):
+		PhysicsServer3D.area_set_param(get_viewport().find_world_3d().space,
+			PhysicsServer3D.AREA_PARAM_GRAVITY_VECTOR, Vector3.RIGHT)
+	else:
+		PhysicsServer3D.area_set_param(get_viewport().find_world_3d().space,
+			PhysicsServer3D.AREA_PARAM_GRAVITY_VECTOR, Vector3.DOWN)
