339b33ca4353ffcb10bf639c87b4cbca7a94f293
enmae\User <mmaring@bell.net>
2025-08-14 16:14:51 -0400

    Renamed group FUN

--- Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/collectables/fish/fish.gd
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/collectables/fish/fish.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 const SCORE = 100.0
 
 func _on_area_3d_body_entered(body: Node3D):
-	if (body.is_in_group("character")):
+	if (body.is_in_group("player")):
 		GameEvents.emit_update_total_score(SCORE)
 		GameEvents.emit_update_fish_count(1)
 		queue_free()
