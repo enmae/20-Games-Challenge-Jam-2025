854d47567ff7771bcc20c4b277662b12ccd8923f
enmae\User <mmaring@bell.net>
2025-08-15 19:15:28 -0400

    Added an initial settings menu (not hooked up correctly to audio); added sfx, music busses to bus layout; modified title screen to include settings and quit options;

---
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/ui/settings/settings_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+signal back_pressed
+
+@onready var master_slider = $%MasterVolumeSlider
+@onready var sfx_slider = $%SFXVolumeSlider
+@onready var music_slider = $%MusicVolumeSlider
+@onready var back_button = $%BackButton
+
+func _ready():
+	back_button.pressed.connect(on_back_button_pressed)
+	master_slider.value_changed.connect(on_audio_slider_changed.bind("master"))
+	sfx_slider.value_changed.connect(on_audio_slider_changed.bind("sfx"))
+	music_slider.value_changed.connect(on_audio_slider_changed.bind("music"))
+
+func set_bus_volume_percent(bus_name: String, percent: float):
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	var volume_db = linear_to_db(percent)
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
+
+func on_audio_slider_changed(value: float, bus_name: String):
+	set_bus_volume_percent(bus_name, value)
+
+func on_back_button_pressed():
+	back_pressed.emit()
