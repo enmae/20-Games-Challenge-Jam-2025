585587c729cb1a0aa06ccf72f942d1b051de5370
enmae\User <mmaring@bell.net>
2025-08-19 21:26:32 -0400

    Made warps work! Updated the greybox level to have a warp point to the level complete area (circle collision box)

---
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/level/warps/warps.gd
@@ -0,0 +1,9 @@
+extends Node3D
+
+func _ready() -> void:
+	$%FirstWarpArea3D.body_entered.connect(on_first_warp_body_entered)
+
+func on_first_warp_body_entered(body: Node3D):
+	if (body.is_in_group("player")):
+		body.position = $%SecondWarp.global_position
+		body.linear_velocity = Vector3.ZERO
