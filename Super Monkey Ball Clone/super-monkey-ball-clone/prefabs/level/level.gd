328166bf696af9078ee362279ad76a2b616735b9
Michael Burt <23486102+bikemurt@users.noreply.github.com>
2025-08-19 22:07:35 -0400

    Character tweaking + rotation behaviour

--- Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/level/level.gd
+++ Super Monkey Ball Clone/super-monkey-ball-clone/prefabs/level/level.gd
@@ -6,13 +6,23 @@ extends Node3D
 
 var angle := 15.0
 var spawn_position := Vector3.ZERO
+var default_speed := 0.4
+var rotaters: Array[MeshInstance3D] = []
 
 func _ready() -> void:
 	spawn_position = character.global_position
 	GameEvents.reset_player_position.connect(on_reset_player_position)
 	GameEvents.last_played_level = get_tree().current_scene.scene_file_path
+	
+	for c in level_node.get_children():
+		if "AM_Moving_Platform" in c.name:
+			rotaters.push_back(c)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	controller_process()
+	rotations_process(delta)
+
+func controller_process() -> void:
 	var _rotate_x := 0.0
 	var _rotate_z := 0.0
 	var tilt_input := Vector2.ZERO
@@ -31,9 +41,6 @@ func _process(_delta: float) -> void:
 	
 	level_node.position = -character.global_position
 	
-	var rx := level_parent.rotation_degrees.x
-	var rz := level_parent.rotation_degrees.z
-	
 	var char_y := character.global_rotation.y
 	var rotated := tilt_input.rotated(char_y)
 	var target_x := rotated.y * angle
@@ -42,6 +49,14 @@ func _process(_delta: float) -> void:
 	level_parent.rotation_degrees.z = lerpf(level_parent.rotation_degrees.z, target_z, 0.1)
 	
 	level_parent.position = character.global_position
+
+func rotations_process(delta: float) -> void:
+	for r in rotaters:
+		var speed := default_speed
+		if r.has_meta("extras"):
+			var extras: Dictionary = r.get_meta("extras")
+			if "speed" in extras: speed = extras["speed"]
+		r.rotation.y += delta * speed
 
 func on_reset_player_position():
 	character.global_position = spawn_position
