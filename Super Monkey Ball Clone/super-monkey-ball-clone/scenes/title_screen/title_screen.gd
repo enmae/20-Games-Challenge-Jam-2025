9901ad4074d737a10fa65f2b928fbd1d1bc4de03
enmae\User <mmaring@bell.net>
2025-08-24 20:19:37 -0400

    Added a button to navigate to the credits on the title screen

--- Super Monkey Ball Clone/super-monkey-ball-clone/scenes/title_screen/title_screen.gd
+++ Super Monkey Ball Clone/super-monkey-ball-clone/scenes/title_screen/title_screen.gd
@@ -7,6 +7,7 @@ var settings_menu = preload("res://prefabs/ui/menus/settings_menu.tscn")
 func _ready() -> void:
 	$%SettingButton.pressed.connect(on_settings_pressed)
 	$%QuitButton.pressed.connect(on_quit_pressed)
+	$%CreditsButton.pressed.connect(on_credits_pressed)
 	
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
@@ -23,3 +24,6 @@ func on_settings_closed(settings_instance: Node):
 
 func on_quit_pressed():
 	get_tree().quit()
+
+func on_credits_pressed():
+	get_tree().change_scene_to_file("res://prefabs/ui/menus/credits_text.tscn")
